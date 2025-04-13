7fe7191421b9ecf9a9424f105f82776a8865bc64
Tarek Poltermann <tarekjpoltermann@gmail.com>
2025-04-14 00:36:38 +0200

    Added erase and write to arduino.ino

--- software/arduino.ino
+++ software/arduino.ino
@@ -1,10 +1,14 @@
-static const uint8_t ADDRESS_PINS[] = {22, 24, 26, 28, 30, 32, 34, 36, 38, 40, 42, 44, 46, 48, 50, 52, 53, 51, 49};  // A-1 (Bit 0) - A17 (Bit 18) (A-1 is the LSB in byte mode)
-static const uint8_t DATA_PINS[] = {23, 25, 27, 29, 31, 33, 35, 37};                                                 // DQ0 - DQ7 (only 8 bits are used in byte mode)
-static const uint8_t RESET_PIN = 3;
-static const uint8_t WE_PIN = 4;
-static const uint8_t OE_PIN = 5;
-static const uint8_t CE_PIN = 6;
-static const uint8_t BYTE_PIN = 7;
+static const uint8_t ADDRESS_PINS[] = {26, 46, 47, 44, 39, 38, 41, 40, 43, 33, 34, 35, 36, 37, 30, 29, 28, 27, 42};  // A-1 (Bit 0) - A17 (Bit 18) (A-1 is the LSB in byte mode)
+static const uint8_t DATA_PINS[] = {51, 50, 53, 52, 22, 23, 24, 25};                                                 // DQ0 - DQ7 (only 8 bits are used in byte mode)
+static const uint8_t RESET_PIN = 31;
+static const uint8_t WE_PIN = 32;
+static const uint8_t OE_PIN = 48;
+static const uint8_t CE_PIN = 49;
+static const uint8_t RY_BY_PIN = 45;
+
+bool is_busy() {
+  return digitalRead(RY_BY_PIN) == LOW;
+}
 
 void set_data(uint8_t data) {
   for (int i = 0; i < 8; i++) {
@@ -26,7 +30,7 @@ void write_byte(uint8_t data, uint32_t address) {
   set_data(data);
 
   digitalWrite(WE_PIN, LOW);
-  delayMicroseconds(30);
+  delayMicroseconds(10);
   digitalWrite(WE_PIN, HIGH);
 }
 
@@ -35,7 +39,7 @@ uint8_t read_byte(uint32_t address) {
   set_address(address);
   delayMicroseconds(10);
   digitalWrite(OE_PIN, LOW);
-  delayMicroseconds(150);
+  delayMicroseconds(50);
 
   uint8_t data = 0;
   for (int i = 0; i < 8; i++) {
@@ -63,7 +67,7 @@ void set_data_out() {
 void setup() {
   Serial.begin(115200);
 
-   // Ready message to host
+  // Ready message to host
   Serial.write(0x99);
 }
 
@@ -73,18 +77,18 @@ void read_ident() {
   write_byte(0xaa, 0xaaa);
   write_byte(0x55, 0x555);
   write_byte(0x90, 0xaaa);
-  delay(100);
+  delayMicroseconds(10);
   set_data_in();
   uint8_t manufacturer_id = read_byte(0x0);
 
   reset_chip();
 
-   // Read device id
+  // Read device id
   set_data_out();
   write_byte(0xaa, 0xaaa);
   write_byte(0x55, 0x555);
   write_byte(0x90, 0xaaa);
-  delay(100);
+  delayMicroseconds(10);
   set_data_in();
   uint8_t device_id = read_byte(0x2);
 
@@ -98,7 +102,7 @@ void read_ident() {
 
 void reset_chip() {
   digitalWrite(RESET_PIN, LOW);
-  delay(1);
+  delayMicroseconds(10);
   digitalWrite(RESET_PIN, HIGH);
 }
 
@@ -109,10 +113,47 @@ void read_data(uint32_t address, uint8_t size) {
   for (uint8_t i = 0; i < size; i++) {
     data[i] = read_byte(address + i);
   }
-  
+
   Serial.write(0x02);  // Message Type
   Serial.write(size);  // Message Length
   Serial.write(data, size);
+}
+
+void erase_sector(uint32_t address) {
+  set_data_out();
+  write_byte(0xaa, 0xaaa);
+  write_byte(0x55, 0x555);
+  write_byte(0x80, 0xaaa);
+  write_byte(0xaa, 0xaaa);
+  write_byte(0x55, 0x555);
+  write_byte(0x30, address);
+  delayMicroseconds(10);
+
+  while (is_busy()) {
+    delayMicroseconds(10);
+  }
+
+  Serial.write(0x03);  // Message Type
+  Serial.write(0x00);  // Message Length
+}
+
+void write_data(uint32_t address, uint8_t size, uint8_t data[]) {
+  set_data_out();
+
+  for (uint8_t i = 0; i < size; i++) {
+    write_byte(0xaa, 0xaaa);
+    write_byte(0x55, 0x555);
+    write_byte(0xa0, 0xaaa);
+    write_byte(data, address + i);
+    delayMicroseconds(10);
+
+    while (is_busy()) {
+      delayMicroseconds(10);
+    }
+  }
+
+  Serial.write(0x04);  // Message Type
+  Serial.write(0x00);  // Message Length
 }
 
 void loop() {
@@ -121,11 +162,11 @@ void loop() {
     uint8_t pre_buffer[2] = {};
     Serial.readBytes(pre_buffer, 2);
     uint8_t message_type = pre_buffer[0];
-    uint16_t message_length = pre_buffer[1];
+    uint8_t message_length = pre_buffer[1];
 
     uint8_t buffer[message_length] = {};
     Serial.readBytes(buffer, message_length);
-    
+
     if (message_type == 0x00) {  // Init
       for (int i = 0; i < 19; i++) {
         pinMode(ADDRESS_PINS[i], OUTPUT);
@@ -133,18 +174,17 @@ void loop() {
       }
 
       set_data_in();
-      
+
       pinMode(RESET_PIN, OUTPUT);
       pinMode(WE_PIN, OUTPUT);
       pinMode(OE_PIN, OUTPUT);
       pinMode(CE_PIN, OUTPUT);
-      pinMode(BYTE_PIN, OUTPUT);
+      pinMode(RY_BY_PIN, INPUT);
 
       digitalWrite(RESET_PIN, HIGH);
       digitalWrite(WE_PIN, HIGH);
       digitalWrite(OE_PIN, HIGH);
       digitalWrite(CE_PIN, LOW);
-      digitalWrite(BYTE_PIN, LOW);
 
       reset_chip();
 
@@ -155,12 +195,20 @@ void loop() {
       read_ident();
 
     } else if (message_type == 0x02) {  // Read Data
-      uint32_t address = ((uint32_t)(uint8_t)buffer[0] << 24) |
-                   ((uint32_t)(uint8_t)buffer[1] << 16) |
-                   ((uint32_t)(uint8_t)buffer[2] << 8)  |
-                   ((uint32_t)(uint8_t)buffer[3]);
+      uint32_t address = ((uint32_t)(uint8_t)buffer[0] << 24) | ((uint32_t)(uint8_t)buffer[1] << 16) | ((uint32_t)(uint8_t)buffer[2] << 8) | ((uint32_t)(uint8_t)buffer[3]);
       uint8_t size = buffer[4];
       read_data(address, size);
+
+    } else if (message_type == 0x03) {  // Erase Sector
+      uint32_t address = ((uint32_t)(uint8_t)buffer[0] << 24) | ((uint32_t)(uint8_t)buffer[1] << 16) | ((uint32_t)(uint8_t)buffer[2] << 8) | ((uint32_t)(uint8_t)buffer[3]); 
+      erase_sector(address);
+
+    } else if (message_type == 0x04) {  // Write Data
+      uint32_t address = ((uint32_t)(uint8_t)buffer[0] << 24) | ((uint32_t)(uint8_t)buffer[1] << 16) | ((uint32_t)(uint8_t)buffer[2] << 8) | ((uint32_t)(uint8_t)buffer[3]); 
+      uint8_t size = buffer[4];
+      uint8_t data[size] = {};
+      memcpy(data, &buffer[5], sizeof(data));
+      write_data(address, size, data);
     }
   }
 }
